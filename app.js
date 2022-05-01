ac97af83583414b6f1e02535c0e8df9b4fab2641
Justin Guo <unoptimalblogs@gmail.com>
2022-05-01 14:02:31 -0700

    pain

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ if(process.env.NODE_ENV !== 'production'){
 
 //express
 const express = require('express')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 const app = express();
 
 //mongoose 
